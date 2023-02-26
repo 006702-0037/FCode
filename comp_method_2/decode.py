c4e167e77e941b221ba1e93e678634e708fe3963
JacobSFreeman <120270131+JacobSFreeman@users.noreply.github.com>
2023-02-25 22:03:48 -0500

    Add files via upload

---
+++ comp_method_2/decode.py
@@ -0,0 +1,35 @@
+from math import ceil
+from encode import color_seq, max_value
+  
+def dec_color_seq(vals, codex, channel_order, max_val=256):
+
+  assert len(codex)==len(channel_order)
+  bases=[ceil(max_val/num) for num in codex]
+  high=max_value(bases, channel_order)+1
+  
+  sum = 0
+  for n in range(len(vals)):
+    total=0
+    mult=1
+    color = vals[n]
+
+    for i in range(len(channel_order)):
+      if i in channel_order:
+        pos=channel_order.index(i)
+        total+=(color[pos]/codex[i])*mult
+        mult*=bases[i]
+    sum+=total*(high**n)
+  return int(sum)
+    
+  
+if __name__ == "__main__":  
+  test_num = 123456789
+  
+  test_codex = [1,1,1]
+  test_order = [-1,0,-1]
+
+  print(f"encoded number: \"{test_num}\"")
+  
+  pixels = color_seq(test_num,test_codex,test_order)
+  out = dec_color_seq(pixels,test_codex,test_order)
+  print(f"decoded number: \"{out}\"")
